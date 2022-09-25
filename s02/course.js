4ad6e2759e6c083b9cfdb579b62028f9c788429d
ArcherFairfax <91679758+ArcherFairfax@users.noreply.github.com>
2022-09-25 21:53:26 +0800

    Add files via upload

---
+++ s02/course.js
@@ -0,0 +1,39 @@
+let course1 = {
+    "_id" : ObjectId("631085a3b92ebab68a3b7dba"),
+    "name" : "Jeanica",
+    "description" : "Student",
+    "price" : 10000,
+    "slots" : 50,
+    "schedule" : "Monday",
+    "instructor" : "Ms. Nika",
+    "isActive" : "Yes",
+    "enrollees" : [ 
+        {
+            "id" : "Enrollment1",
+            "userId" : "63108322b92ebab68a3b7d07",
+            "userName(optional)" : "Nika",
+            "isPaid" : "yes",
+            "dateEnrolled" : "09/01/2022"
+        }
+    ]
+};
+
+let course2 = {
+    "_id" : ObjectId("63108843b92ebab68a3b7ea6"),
+    "name" : "Niks",
+    "description" : "Student",
+    "price" : 10000,
+    "slots" : 50,
+    "schedule" : "Monday",
+    "instructor" : "Ms. Conchada",
+    "isActive" : "Yes",
+    "enrollees" : [ 
+        {
+            "id" : "Enrollment1",
+            "userId" : "631087c3b92ebab68a3b7e7f",
+            "userName(optional)" : "Neks",
+            "isPaid" : "yes",
+            "dateEnrolled" : "09/01/2022"
+        }
+    ]
+};
---
+++ s02/users.js
@@ -0,0 +1,38 @@
+let user1 = {
+    "_id" : ObjectId("63108322b92ebab68a3b7d07"),
+    "username" : "nika05",
+    "firstName" : "nika",
+    "lastName" : "conchada",
+    "email" : "conchadajeanica@gmail.com",
+    "password" : "niks0427",
+    "mobileNumber" : "09284939502",
+    "isAdmin" : "false",
+    "enrollments" : [ 
+        {
+            "id" : "Enrollment1",
+            "courseId" : "631085a3b92ebab68a3b7dba",
+            "courseName" : "BS-IT",
+            "isPaid" : "Yes",
+            "dateEnrolled" : "09/01/2022"
+        }
+    ]
+};
+let user2 = {
+    "_id" : ObjectId("631087c3b92ebab68a3b7e7f"),
+    "username" : "nika28",
+    "firstName" : "nika",
+    "lastName" : "conchada",
+    "email" : "niksconch@gmail.com",
+    "password" : "conch72",
+    "mobileNumber" : "09938503848",
+    "isAdmin" : "false",
+    "enrollments" : [ 
+        {
+            "id" : "Enrollment2",
+            "courseId" : "63108843b92ebab68a3b7ea6",
+            "courseName" : "BS-IT",
+            "isPaid" : "Yes",
+            "dateEnrolled" : "09/01/2022"
+        }
+    ]
+};
